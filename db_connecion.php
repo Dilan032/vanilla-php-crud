0a336241efc05d9f45996698a6cd91ab4fa7e5cf
Dilan kanishka <dilankanishka032@gmail.com>
2025-08-21 18:34:08 +0530

    connect to DB

---
+++ db_connecion.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn =mysqli_connect("localhost", "root", "", "php_crud");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}else {
+    // Connection successful
+    // echo "Connected successfully";
+}
+
+
+?>
